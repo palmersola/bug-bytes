0f4f4dab8a9b4059db0a8aaabff842171b8a46f9
unknown <darrandaf@gmail.com>
2023-05-02 14:07:07 -0400

    keyword search function

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ class AllPosts {
   update() {}
   delete() {}
 }
+
+// Create a function to search keywords
+function search(keywords) {
+// Get current posts
+  const posts = document.querySelectorAll('.post');
+// Filter
+  const filteredPosts = Array.from(posts).filter(post => {
+    return keywords.some(keyword => post.textContent.includes(keyword));
+  });
+
+// Return filtered
+    return filteredPosts;
+}
+
+// Event listener for search button
+document.getElementById('searchButton').addEventListener('click', () => {
+// Get keywords from the search
+  const keywords = document.getElementById('searchInput').value;
+
+// Search for posts with keywords
+  const filteredPosts = search(keywords);
+
+// Clear the existing
+  document.getElementById('posts').innerHTML = '';
+
+// Add the filtered posts
+  for (const post of filteredPosts) {
+    document.getElementById('posts').appendChild(post);
+  }
+});
