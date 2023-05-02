cf5428264d2745a7dd935d73c6fdeeaffae6ce00
palmersola <palmersola@yahoo.com>
2023-05-02 09:29:14 -0500

    added id

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Post {
   constructor(content, author, tags) {
+    this.id = Math.round(new Date().getTime() + Math.random() * 100000);
     this.content = content;
     this.date = new Date();
     this.author = author;
