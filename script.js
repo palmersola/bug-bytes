7bab1c65cef355236e5885554368759b6ae773cc
Lee Vue <vuelee123@gmail.com>
2023-05-02 14:05:09 -0400

    create post

--- script.js
+++ script.js
@@ -20,5 +20,23 @@ class AllPosts {
 
 function create () {
   const input = document.getElementById("post").ariaValueMax.trim();
+  if (input !== "") {
+    if (input.length <=250) {
+      posts.push(input);
+      showPosts ();
 
+      document.getElementById("post").value = " "; 
+      document.getElementById("count").textContent = "250";
+    } else {
+      alert("Your tweet exceeded the character amount.");
+    }
+  } else {
+    alert("Tweet something...");
+  }
 }
+
+document.getElementById("post").addEventListener("input", function() {
+  const input = this.value;
+  const count = document.getElementById("charCount");
+  count.textContent = 150 - input.length;
+});
