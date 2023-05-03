c3fb14fafcc32de5d4de03a1b09d3ac7af6f341c
palmersola <palmersola@yahoo.com>
2023-05-03 09:06:00 -0500

    prepping merge

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ class Post {
   constructor(content, author, tags) {
     this.id = Math.round(new Date().getTime() + Math.random() * 100000);
     this.content = content;
-    this.date = new Date();
+    this.date = new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric"
+    });
     this.author = author;
     this.tags = tags;
   }
@@ -14,9 +21,15 @@ class AllPosts {
   }
   create(content, author, tags) {
     this.postList.push(new Post(content, author, tags));
+    localStorage.setItem("posts", JSON.stringify(this.postList));
   }
 
   read() {
+    // localStorage.getItem("posts")
+    //   ? (this.postList = JSON.parse(localStorage.getItem("posts")))
+    //   : localStorage.setItem("posts", JSON.stringify([]));
+    // return this.postList;
+
     let posts = [];
     let keys = Object.keys(localStorage);
     for (let i = 0; i < keys.length; i++) {
@@ -29,20 +42,25 @@ class AllPosts {
     return this.postList;
   }
 
-  update() {}
-  delete() {}
+  update(id) {
+    console.log(id);
+  }
+  delete(id) {
+    this.postList = this.postList.filter(post => post.id !== id);
+    displayAllPosts();
+  }
 }
 
 let allPosts = new AllPosts();
 
 //DOM manipulation to show all posts
-function displayAllPosts() {
+function displayAllPosts(search) {
   let postList = document.getElementById("listOfPosts");
   postList.innerHTML = "";
-  // let posts = allPosts.read();
-
-  for (let i = 0; i < allPosts.postList.length; i++) {
-    let post = allPosts.postList[i];
+  let posts = allPosts.read();
+  let list = search ? search : posts;
+  for (let i = 0; i < list.length; i++) {
+    let post = list[i];
     let postElement = document.createElement("div");
 
     postElement.innerHTML = `<div>
@@ -52,20 +70,22 @@ function displayAllPosts() {
     <div>
     <span class="postContent">${post.content}</span>
     <span class="postTags">${post.tags}</span>
+    <button onclick="allPosts.delete(${post.id})">DELETE</button>
+    <button onclick="allPosts.update(${post.id})">UPDATE</button>
     </div>`;
 
     postList.appendChild(postElement);
   }
-  console.log(allPosts.postList);
 }
 
 function create() {
   const content = document.getElementById("content").value.trim();
   const author = document.getElementById("author").value.trim();
-  const tags = document.getElementById("tags").value;
+  const tags = document.getElementById("tags").value.trim();
   if (content !== "") {
     if (content.length <= 150) {
       allPosts.create(content, author, tags);
+      console.log(allPosts.postList);
       displayAllPosts();
 
       document.getElementById("content").value = " ";
@@ -78,41 +98,20 @@ function create() {
   }
 }
 
+displayAllPosts();
+
 // document.getElementById("content").addEventListener("input", function() {
 //   const input = this.value;
 //   const count = document.getElementById("charCount");
 //   count.textContent = 150 - input.length;
 // });
 
-// Create a function to search keywords
-function search(keywords) {
-  // Get current posts
-  // const posts = document.querySelectorAll(".post");
-  // Filter
-  let filteredPosts = allPosts.postList.filter(post => {
-    console.log(keywords);
-    console.log(post.content);
-    post.content === "test";
-    // return keywords.some(keyword => post.includes(keywords));
-  });
-  console.log(filteredPosts);
-  // Return filtered
-  return filteredPosts;
-}
-
 // Event listener for search button
 document.getElementById("searchButton").addEventListener("click", () => {
   // Get keywords from the search
   const keywords = document.getElementById("searchInput").value;
-
   // Search for posts with keywords
-  const filteredPosts = search(keywords);
+  const filteredPosts = allPosts.postList.filter(post => post.tags == keywords);
 
-  // Clear the existing
-  document.getElementById("listOfPosts").innerHTML = "";
-
-  // Add the filtered posts
-  for (const post of filteredPosts) {
-    document.getElementById("listOfPosts").appendChild(post);
-  }
+  displayAllPosts(filteredPosts);
 });
